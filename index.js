0b4f8b0a75579c4f4c73d525f07154e38d67fd58
Holly Rogers <hollyrogers@Hollys-Air.lan>
2019-12-29 14:50:09 -0500

    added js file and hamburger

---
+++ index.js
@@ -0,0 +1,24 @@
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+
+//debug later
+window.onClick = function(event) {
+    if(!event.target.matches(".dropbtn")) {
+        let dropdowns = document.getElementsByClassName("dropdown-content");
+        let i;
+        for (i = 0; i < dropdowns.length; i++) {
+            let openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains("show")){
+                openDropdown.classList.remove("show");
+                console.log('removing');
+            }
+        }
+    }
+}
+// $(function($){
+//     $(".menuBtn").click(function(){
+//         $(".responsive-menu").toggleClass("expand")
+//     })
+// })
