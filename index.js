df060a3e754d095fe315e35f83453006db790d4d
Holly Rogers <hollyrogers@Hollys-Air.lan>
2019-12-31 10:29:46 -0500

    dropdown aligned and js click working

--- index.js
+++ index.js
@@ -1,24 +1,7 @@
-function myFunction() {
+$(".dropbtn").on("click", function () {
     document.getElementById("myDropdown").classList.toggle("show");
-}
+})
 
-
-//debug later
-window.onclick = function(event) {
-    if(!event.target.matches(".dropbtn")) {
-        let dropdowns = document.getElementsByClassName("dropdown-content");
-        let i;
-        for (i = 0; i < dropdowns.length; i++) {
-            let openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains("show")){
-                openDropdown.classList.remove("show");
-                console.log('removing');
-            }
-        }
-    }
-}
-// $(function($){
-//     $(".menuBtn").click(function(){
-//         $(".responsive-menu").toggleClass("expand")
-//     })
-// })
+$("#dropdown-list").on("click", function () {
+    document.getElementsByClassName("dropList").classList.toggle("hover");
+})
